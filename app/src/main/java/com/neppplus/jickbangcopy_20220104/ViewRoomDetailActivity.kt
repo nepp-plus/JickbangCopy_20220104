a54a5cff0f6af2ccef0c6c730c86aa2a4730ef53
nepp-plus <nepp.plus@gmail.com>
2022-01-04 16:44:24 +0900

    방 상세보기 화면에서, 보내준 RoomData 받아내기   - Serializable 형태로 보낸것을 -> as RoomData 로 원복

--- app/src/main/java/com/neppplus/jickbangcopy_20220104/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220104/ViewRoomDetailActivity.kt
@@ -2,10 +2,17 @@ package com.neppplus.jickbangcopy_20220104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20220104.models.RoomData
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val roomData = intent.getSerializableExtra("room") as RoomData  // Serializable 첨부된것을 -> RoomData 원복
+
+
+
+
     }
 }
